ab5bbe5b3b079ae45693bebc2aa58a03a03c0f6e
Igor Dobryn <IgorDobryn@gmail.com>
2011-08-13 01:52:05 +0400

    Completed ex_2_7-2_10

---
+++ ex_2_10.rkt
@@ -0,0 +1,81 @@
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
+(define (make-interval a b) 
+  (cons a b)
+)
+
+(define  (add-interval x y)
+  (make-interval  (+ (lower-bound x) (lower-bound y))
+                  (+ (upper-bound x) (upper-bound y))
+  )
+)
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+  
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4)
+    )
+  )
+)
+
+(define (div-interval x y)
+   (if 
+      (or (=  (upper-bound y) 0)
+          (=  (lower-bound y) 0))
+      ((display "Division by 0!"))
+
+      (mul-interval x
+          (make-interval (/ 1.0 (upper-bound y))
+                         (/ 1.0 (lower-bound y))
+          )
+      )
+  )
+)
+
+(define (sub-interval x y)
+  (make-interval 
+        (- (lower-bound x) (lower-bound y)  )
+        (- (upper-bound x) (upper-bound y)  )    
+  )
+)
+
+(define (interval-width x)
+  (/ (- (upper-bound x) 
+        (lower-bound x)
+     )
+   2
+  )
+)
+
+
+(define (print-interval x)
+  (newline)
+  (display "(")
+  (display (lower-bound x))
+  (display "; ")
+  (display (upper-bound x))
+  (display ")")
+    
+)
+
+(define R1    (make-interval 6.12 7.48))
+(define R2    (make-interval 0 2.97))
+(define R1-R2 (sub-interval R1 R2))
+(define R1+R2 (add-interval R1 R2))
+(define R1*R2 (mul-interval R1 R2))
+(define R1/R2 (div-interval R1 R2))
+
+(newline)
+(display "Intervals")
+(newline)
+(print-interval R1)
+(print-interval R2)
+(print-interval R1-R2)
+(print-interval R1+R2)
+(print-interval R1*R2)
+(print-interval R1/R2)
---
+++ ex_2_7.rkt
@@ -0,0 +1,52 @@
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
+(define (make-interval a b) 
+  (cons a b)
+)
+
+(define  (add-interval x y)
+  (make-interval  (+ (lower-bound x) (lower-bound y))
+                  (+ (upper-bound x) (upper-bound y))
+  )
+)
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+  
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4)
+    )
+  )
+)
+
+(define (div-interval x y)
+  (mul-interval x
+        (make-interval (/ 1.0 (upper-bound y))
+                       (/ 1.0 (lower-bound y))
+        )
+  )
+)
+
+(define (print-interval x)
+  (newline)
+  (display "(")
+  (display (lower-bound x))
+  (display "; ")
+  (display (upper-bound x))
+  (display ")")
+    
+)
+
+(define R1 (make-interval 6.12 7.48))
+(define R2 (make-interval 2.58 2.97))
+
+(print-interval R1)
+(print-interval R2)
+
+(print-interval (add-interval R1 R2))
+(print-interval (mul-interval R1 R2))
+(print-interval (div-interval R1 R2))
