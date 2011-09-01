0efa451dab542ce92fb63a20c7788b47fb70b934
Igor Dobryn <IgorDobryn@gmail.com>
2011-09-01 23:57:24 +0400

    fixed. added tests

--- ex_1_012.scm
+++ ex_1_012.scm
@@ -1,15 +1,20 @@
-(define (get-Paskal-triangle-value row column)
-  (cond ((and (= row 0)
-              (= column 0))
-          1)
-        (else (get-parent row column))))
+(require rackunit)
+
+(define (get-paskal-triangle-value row column)
+        (get-parent row column))
 
 (define (get-parent row column)
-  (cond ((or (= row 0)
+  (cond ((or (= column (+ row 1))
              (= column 0))
           1)
         (else (+ (get-parent (- row 1) column)
-                 (get-parent row (- column 1))))))
+                 (get-parent (- row 1) (- column 1))))))
 
 
-(get-Paskal-triangle-value 3 2)
+(check-equal? (get-paskal-triangle-value 4 0) 1)
+(check-equal? (get-paskal-triangle-value 1 2) 1)
+
+(check-equal? (get-paskal-triangle-value 2 1) 3)
+(check-equal? (get-paskal-triangle-value 2 2) 3)
+(check-equal? (get-paskal-triangle-value 5 2) 15)
+(check-equal? (get-paskal-triangle-value 4 2) 10)
