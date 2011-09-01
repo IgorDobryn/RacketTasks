5d356c277ce9ce4bb1bed5ae36a0c2b1dec0be32
Igor Dobryn <IgorDobryn@gmail.com>
2011-09-01 20:39:31 +0400

    Fixed. Added test.

--- ex_1_003.scm
+++ ex_1_003.scm
@@ -1,7 +1,18 @@
-(define (bigest-num-sum-squares a b c)
-  (cond ((and (< a b) (< a c))
-         (+ (* b b) (* c c)))
-        ((and (< b a) (< b c))
-         (+ (* a a) (* c c)))
+(require rackunit)
+
+(define (square x)
+  (* x x))
+
+(define (biggest-num-sum-squares a b c)
+  (cond ((and (<= a b) (<= a c))
+         (+ (square b) (square c)))
+        ((and (< b a) (<= b c))
+         (+ (square a) (square c)))
         ((and (< c b) (< c a))
-         (+ (* b b) (* a a)))))
+         (+ (square b) (square a)))))
+
+
+(check-equal? (biggest-num-sum-squares 3 1 0) 10)
+(check-equal? (biggest-num-sum-squares 4 1 2) 20)
+(check-equal? (biggest-num-sum-squares 1 0 0) 1)
+(check-equal? (biggest-num-sum-squares 0 0 0) 0)
