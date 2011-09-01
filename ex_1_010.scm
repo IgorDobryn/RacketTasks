3d7ad8a18138d90e547d3471b8575699fa7400ef
Igor Dobryn <IgorDobryn@gmail.com>
2011-09-01 23:20:00 +0400

    fixed. added tests

--- ex_1_010.scm
+++ ex_1_010.scm
@@ -1,3 +1,5 @@
+(require rackunit)
+
 (define (A x y)
   (cond ((= y 0) 0)
         ((= x 0) (* 2 y))
@@ -5,10 +7,18 @@
         (else (A (- x 1)
                  (A x (- y 1))))))
 
-;(A 1 10) = 1024
-;(A 2 4) = 65536
-;(A 3 3) = 65536
-
 (define (f n) (A 0 n)); вычисляет 2*n
 (define (g n) (A 1 n)); вычисляет 2^n
-(define (h n) (A 2 n)); вычисляет 2^(n^2)
+(define (h n) (A 2 n)); вычисляет 2^(2^n)
+
+
+(check-equal? (f 2) 4)
+(check-equal? (f 10) 20)
+
+(check-equal? (g 2) 4)
+(check-equal? (g 10) 1024)
+
+(check-equal? (h 1) 2)
+(check-equal? (h 2) 4)
+(check-equal? (h 3) 16)
+(check-equal? (h 4) 65536)
