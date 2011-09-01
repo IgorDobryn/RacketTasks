6f71043017a151c5842c42dbc7e94c94f1aae064
Igor Dobryn <IgorDobryn@gmail.com>
2011-09-02 01:42:51 +0400

    completed 1_015

---
+++ ex_1_015.scm
@@ -0,0 +1,22 @@
+(define (cube x) (* x x x))
+
+(define (p x) (- (* 3 x) (* 3 (cube x))))
+
+(define (sine angle)
+  (display "1")
+  (newline)
+  (if (not (> (abs angle) 0.1))
+    angle
+    (p (sine (/ angle 3.0)))))
+
+(sine 12.15)
+;а:
+; 1. angle = 12.15
+; 2. angle = 4.05
+; 3. angle = 1.35
+; 4. angle = 0.45
+; 5. angle = 0.15
+; 6. angle = 0.05
+;6 раз
+;б  порядок роста в терминах количества шагов - O(2/3*n)
+;в терминах используемой памяти O(1)
